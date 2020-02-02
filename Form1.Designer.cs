49f9e68a0ec2651817f4785e1c697f29f6805489
Leandro Hereñú <leandrohere94@live.com.ar>
2020-02-02 12:00:49 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AutomatedTestSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTestClassName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 43);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(776, 395);
+            this.txtCode.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Paste your class structucture and click generate to get your test";
+            // 
+            // txtTestClassName
+            // 
+            this.txtTestClassName.Location = new System.Drawing.Point(325, 13);
+            this.txtTestClassName.Name = "txtTestClassName";
+            this.txtTestClassName.ReadOnly = true;
+            this.txtTestClassName.Size = new System.Drawing.Size(313, 20);
+            this.txtTestClassName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTestClassName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTestClassName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AutomatedTestSetup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Add Moq, XUnit and FluentAssertions usings
+            var code = "" +
+                "using Moq;\n" +
+                "using XUnit;\n" +
+                "using FluentAssertions;\n";
+
+            var testClassName = string.Empty;
+            var className = string.Empty;
+            var injections = new Dictionary<string, string>();
+
+            for (int i = 0; i < txtCode.Lines.Length; i++)
+            {
+                var line = txtCode.Lines[i].Trim();
+
+                //the class to test
+                if (line.Contains("class"))
+                {
+                    var interfaceStart = line.IndexOf(':');
+                    line = line.Remove(interfaceStart).Trim();
+
+                    //public class removed
+                    className = line.Remove(0, 12).Trim();
+                    testClassName = className + "Test";
+
+                    code += line + "Test" + "\n";
+                    continue;
+                }
+
+                //injections
+                if (line.StartsWith("private readonly"))
+                {
+                    //private readonly removed
+                    line = line.Remove(0, 16).Trim();
+
+                    //separate interface from variable name
+                    //removing semi colon
+                    var injection = line.Remove(line.Length-1).Split(' ');
+
+                    injection[0] = "Mock<" + injection[0] + ">";
+
+                    //key = variable name
+                    injections.Add(injection[1], injection[0]);
+                    
+                    code += "private readonly " + injection[0] + " " + injection[1] + ";\n";
+                    continue;
+                }
+
+                //constructor reached
+                if (line.Contains("public " + className))
+                {
+                    code += "\nprivate readonly " + className + " " + className.ToLower() + ";\n\n";
+
+                    break;
+                }
+
+                //using or { } or namespace or ' '
+                code += line + "\n";
+            }
+
+            //creation of test class constructor
+            code += "public " + testClassName + "()\n";
+            code += "{\n";
+
+            foreach (var item in injections)
+            {
+                //initilize mock object
+                code += "this." + item.Key + " = new " + item.Value + "();\n";
+            }
+
+            //create class to test
+            code += "\nthis." + className.ToLower() + " = new " + className + "(\n";
+
+            for (int i = 0; i < injections.Count; i++)
+            {
+                //inject mock object
+                if(i < injections.Count - 1)
+                {
+                    code += "\tthis." + injections.ElementAt(i).Key + ".Object,\n";
+                }
+                else
+                {
+                    code += "\tthis." + injections.ElementAt(i).Key + ".Object);\n";
+                }
+            }
+
+            //close constructor
+            code += "}\n";
+
+            //create example test
+            code += "\n[Fact]\n";
+            code += "public void Test()\n";
+            code += "{\n";
+
+            //close test, class and namespace
+            code += "}\n}\n}\n";
+
+            txtCode.Text = code;
+            txtTestClassName.Text = testClassName;
+        }
+    }
+}
